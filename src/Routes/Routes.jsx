07d12e094c8c8fe99cbd43299db92a30d2d1b3bc
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-03-27 15:08:42 +0600

    basic routes created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/donation",
-    element: <div>Donation</div>,
-  },
-  {
-    path: "/statistics",
-    element: <div>Statistics</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/donation",
+        element: <Donation />,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics />,
+      },
+    ],
   },
 ]);
 
